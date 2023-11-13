3b3e997ef883d8a0412de0e4d218e4450116d752
Nanzoboy <renan.pimentel005@gmail.com>
2023-11-13 16:05:40 +0000

    Landing page restaurante utilizando html, css e js

---
+++ main.js
@@ -0,0 +1,43 @@
+let current =1
+
+const slideShow = () => {
+    const slides = Array.from(document.querySelectorAll(".slide"))
+
+    if(current > slides.length){
+        current = 1
+    } else if(current === 0){
+        current = slides.length
+    }
+
+    slides.forEach(slide => {
+        if(slide.classList[1].split("-")[1] * 1 === current){
+            slide.style.cssText = "visibility: visible; opacity: 1"
+        } else {
+            slide.style.cssText = "visibility: hidden; opacity: 0"
+        }
+    })
+
+    current++
+}
+
+slideShow()
+setInterval(() => {
+    slideShow();
+}, 2000)
+
+const sidebarBtn =document.querySelector('.sidebar-btn')
+const sidebar =document.querySelector('.sidebar')
+const menuIcon =document.querySelector('.menu-icon')
+const closeBtn =document.querySelector('.sidebar i')
+
+menuIcon.addEventListener("click", ()=>{
+    sidebar.classList.add("navigate")
+})
+
+closeBtn.addEventListener("click", ()=>{
+    sidebar.classList.remove("navigate")
+})
+
+sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("change")
+})
